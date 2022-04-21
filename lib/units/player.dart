31ce42931617b65d4376d16b24c4b5c1a01b3343
Georgy <george@focus21.io>
2022-04-21 15:09:21 +0400

    jumping animation implemented

--- lib/units/player.dart
+++ lib/units/player.dart
@@ -29,6 +29,7 @@ class Player extends SpriteAnimationComponent
   bool isOnGround = true;
   final Vector2 _up = Vector2(0, -1);
   double _animationSpeed = 0.3;
+  late SpriteAnimation currentAnimation;
   late Vector2 _objectSize = Vector2(_radius, _radius);
   Vector2 velocity = Vector2.zero();
   int _xAxisInput = 0;
@@ -53,33 +54,54 @@ class Player extends SpriteAnimationComponent
     super.onCollision(intersectionPoints, other);
   }
 
+  Async.Future<void> checkAnimation() async {
+    print(velocity.y);
+    bool isHover = velocity.y <= 50 && velocity.y >= 0;//velocity.y >= -10 && velocity.y <= 10;
+    if (isOnGround) {
+      if (velocity.x > 0) {
+        await animateRunRight();
+      } else if (velocity.x == 0) {
+        await animateStay();
+      } else {
+        await animateRunLeft();
+      }
+    } else {
+
+      if (velocity.y < 0) {
+        await animateJump();
+        if (velocity.y >=-200) {
+          await animateHover();
+        }
+      } else {
+        await animateFall();
+        if (velocity.y <=50) {
+          await animateHover();
+        }
+      }
+    }
+  }
+
   void movePlayer({JoystickDirection direction = JoystickDirection.idle}) {
     if (direction == JoystickDirection.right) {
       _xAxisInput = 1;
-      animateRunRight();
     }
     if (direction == JoystickDirection.upRight) {
       _xAxisInput = 1;
       _yAxisInput = 1;
-      animateRunRight();
     }
     if (direction == JoystickDirection.left) {
       _xAxisInput = -1;
-      animateRunLeft();
     }
     if (direction == JoystickDirection.upLeft) {
       _xAxisInput = -1;
       _yAxisInput = 1;
-      animateRunLeft();
     }
     if (direction == JoystickDirection.idle) {
       _xAxisInput = 0;
       _yAxisInput = 0;
-      animateStay();
     }
     if (direction == JoystickDirection.up) {
       _yAxisInput = 1;
-      // animateJump(); //todo implement jump
     }
   }
 
@@ -95,7 +117,8 @@ class Player extends SpriteAnimationComponent
     _objectSize = Vector2(_radius, _radius);
     size = _objectSize;
     anchor = Anchor.center;
-    await animateStay();
+    await checkAnimation();
+    animation = currentAnimation;
     addHitbox(HitboxRectangle(relation: Vector2(0.52, 1)));
     return super.onLoad();
   }
@@ -103,16 +126,25 @@ class Player extends SpriteAnimationComponent
   @override
   void update(double dt) {
     super.update(dt);
+    checkAnimation();
+    if (!animation!
+        .getSprite()
+        .image
+        .isCloneOf(currentAnimation.getSprite().image)) {
+      animation = currentAnimation;
+    }
     velocity.x = _xAxisInput * _speed;
     velocity.y = velocity.y.clamp(-(_speed * 2), 350);
     velocity.y += _gravity;
-
     if (_yAxisInput == 1) {
       if (isOnGround) {
         velocity.y = -(_speed * 2);
         isOnGround = false;
       }
       _yAxisInput = 0;
+    }
+    if (velocity.y > 0) {
+      isOnGround = false;
     }
     position += velocity * dt;
     boundPosition();
@@ -128,6 +160,37 @@ class Player extends SpriteAnimationComponent
     );
   }
 
+  Async.Future<void> animateJump() async {
+    _animationSpeed = 0.2;
+    double spriteSize = 96;
+    String spritePath = Assets.playerJump;
+    var spriteSheet = await gameRef.images.load(spritePath);
+    SpriteSheet _spriteSheet = SpriteSheet(
+        image: spriteSheet, srcSize: Vector2(spriteSize, spriteSize));
+    currentAnimation = _spriteSheet.createAnimation(
+        row: 0, from: 1, to: 4, stepTime: _animationSpeed);
+  }
+
+  Async.Future<void> animateFall() async {
+    _animationSpeed = 0.2;
+    double spriteSize = 96;
+    String spritePath = Assets.playerFall;
+    var spriteSheet = await gameRef.images.load(spritePath);
+    SpriteSheet _spriteSheet = SpriteSheet(
+        image: spriteSheet, srcSize: Vector2(spriteSize, spriteSize));
+    currentAnimation = _spriteSheet.createAnimation(
+        row: 0, from: 5, to: 6, stepTime: _animationSpeed);
+  }
+  Async.Future<void> animateHover() async {
+    _animationSpeed = 0.2;
+    double spriteSize = 96;
+    String spritePath = Assets.playerHover;
+    var spriteSheet = await gameRef.images.load(spritePath);
+    SpriteSheet _spriteSheet = SpriteSheet(
+        image: spriteSheet, srcSize: Vector2(spriteSize, spriteSize));
+    currentAnimation = _spriteSheet.createAnimation(
+        row: 0, from: 4, to: 5, stepTime: _animationSpeed);
+  }
   Async.Future<void> animateStay() async {
     _animationSpeed = 0.3;
     double spriteSize = 96;
@@ -135,28 +198,29 @@ class Player extends SpriteAnimationComponent
     var spriteSheet = await gameRef.images.load(spritePath);
     SpriteSheet _spriteSheet = SpriteSheet(
         image: spriteSheet, srcSize: Vector2(spriteSize, spriteSize));
-    animation = _spriteSheet.createAnimation(row: 0, stepTime: _animationSpeed);
+    currentAnimation =
+        _spriteSheet.createAnimation(row: 0, stepTime: _animationSpeed);
   }
 
   Async.Future<void> animateRunRight() async {
-    _animationSpeed = 0.15;
+    _animationSpeed = 0.3;
     double spriteSize = 96;
     String spritePath = Assets.playerMove;
     var spriteSheet = await gameRef.images.load(spritePath);
     SpriteSheet _spriteSheet = SpriteSheet(
         image: spriteSheet, srcSize: Vector2(spriteSize, spriteSize));
-    animation = _spriteSheet.createAnimation(
+    currentAnimation = _spriteSheet.createAnimation(
         row: 0, stepTime: _animationSpeed, from: 0, to: 2);
   }
 
   Async.Future<void> animateRunLeft() async {
-    _animationSpeed = 0.15;
+    _animationSpeed = 0.3;
     double spriteSize = 96;
     String spritePath = Assets.playerMove;
     var spriteSheet = await gameRef.images.load(spritePath);
     SpriteSheet _spriteSheet = SpriteSheet(
         image: spriteSheet, srcSize: Vector2(spriteSize, spriteSize));
-    animation = _spriteSheet.createAnimation(
+    currentAnimation = _spriteSheet.createAnimation(
         row: 0, stepTime: _animationSpeed, from: 2, to: 4);
   }
 }
--- lib/utils/configs.dart
+++ lib/utils/configs.dart
@@ -2,6 +2,9 @@ abstract class Assets {
   static const level1 = 'level-1.tmx';
   static const playerStay = 'hero-stay.png';
   static const playerMove = 'hero-move.png';
+  static const playerJump = 'hero-jump.png';
+  static const playerFall = 'hero-fall.png';
+  static const playerHover = 'hero-hover.png';
 }
 
 abstract class Tiles {
